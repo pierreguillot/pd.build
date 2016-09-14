e4624c946768f5e2129e86be0d9ff078272f118d
pierreguillot <guillotpierre6@gmail.com>
2016-09-14 16:21:58 +0200

    update

--- pd.cmake
+++ pd.cmake
@@ -1,6 +1,10 @@
 
 # The path to this file.
 set(PD_CMAKE_PATH ${CMAKE_CURRENT_LIST_DIR})
+# The path to Pure Data sources.
+set(PD_SOURCES_PATH)
+# The output path for the externals.
+set(PD_SOURCES_PATH)
 
 # The function adds an external to the project.
 # PROJECT_NAME is the name of your project (for example: freeverb_project)
@@ -12,7 +16,8 @@ set(PD_CMAKE_PATH ${CMAKE_CURRENT_LIST_DIR})
 function(add_pd_external PROJECT_NAME EXTERNAL_NAME EXTERNAL_SOURCES)
 	source_group(src FILES ${EXTERNAL_SOURCES})
 	add_library(${PROJECT_NAME} SHARED ${EXTERNAL_SOURCES})
-	
+	target_include_directories(${PROJECT_NAME} PRIVATE ${PD_SOURCES_PATH})
+
 	string(FIND ${EXTERNAL_NAME} "." NAME_HAS_DOT)
 	string(FIND ${EXTERNAL_NAME} "~" NAME_HAS_TILDE)
 	if((CMAKE_GENERATOR STREQUAL Xcode) AND (NAME_HAS_DOT EQUAL -1) AND (NAME_HAS_TILDE GREATER -1))
@@ -31,6 +36,7 @@ function(add_pd_external PROJECT_NAME EXTERNAL_NAME EXTERNAL_SOURCES)
 		set_target_properties(${PROJECT_NAME} PROPERTIES SUFFIX ".dll")
 		target_link_libraries(${PROJECT_NAME} pd)
 		target_include_directories(${PROJECT_NAME} PRIVATE ${PD_CMAKE_PATH})
+		message(STATUS ${PD_CMAKE_PATH})
 		target_compile_definitions(${PROJECT_NAME} PRIVATE "/D_CRT_SECURE_NO_WARNINGS /wd4091 /wd4996")
 	endif()
 endfunction(add_pd_external)
@@ -50,5 +56,5 @@ endmacro(set_pd_external_path)
 
 # The macro sets the location of Pure Data sources.
 macro(set_pd_sources PD_SOURCES)
-	include_directories(${PD_SOURCES})
+	set(PD_SOURCES_PATH ${PD_SOURCES})
 endmacro(set_pd_sources)
