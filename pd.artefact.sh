ed0e3f74972c278a343ee257ed53945b7aada41e
pierreguillot <guillotpierre6@gmail.com>
2016-09-14 18:48:21 +0200

    remove scripts

--- pd.artefact.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ "$os" == "Visual Studio 2015" ]; then
-  del /F /S /Q /A %APPVEYOR_BUILD_FOLDER%\*.pdb
-  del /F /S /Q /A %APPVEYOR_BUILD_FOLDER%\*.ilk
-fi
--- pd.install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$os" = "linux" ] && [ "$plateform" = "x86" ]; then
-  sudo apt-get install -y gcc-multilib
-  sudo apt-get install -y libc6-dev-i386
-elif [ "$os" == "Visual Studio 2015" ]; then
-  git submodule update --init --recursive
-fi
