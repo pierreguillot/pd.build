c2acd8d5745dea865fd78205fd01dd8497907299
pierreguillot <guillotpierre6@gmail.com>
2016-11-29 17:30:14 +0100

    update package

--- ci.package.sh
+++ ci.package.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cp LICENSE PACKAGE/license.txt
-cp README.md PACKAGE/readme.txt
-cp -r src/ PACKAGE/sources/
+zip -r $PACKAGE.zip $*
 
 PACKAGE_VALUE=$TRAVIS_COMMIT
 if [ $TRAVIS_TAG ]; then
@@ -12,15 +10,15 @@ fi
 if [[ "$TRAVIS" == "true" ]]; then
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 if [[ "$PLATFORM" == "linux32" ]]; then
-  zip -r $PACKAGE-$PACKAGE_VALUE-"("Linux-i386-32")""("Sources")"-externals.zip $PACKAGE
+  mv $PACKAGE.zip $PACKAGE-$TRAVIS_COMMIT-"("Linux-i386-32")""("Sources")"-externals.zip
 elif [[ "$PLATFORM" == "linux64" ]]; then
-  zip -r $PACKAGE-$PACKAGE_VALUE-"("Linux-amd64-64")""("Sources")"-externals.zip $PACKAGE
+  mv $PACKAGE.zip $PACKAGE-$TRAVIS_COMMIT-"("Linux-amd64-64")""("Sources")"-externals.zip
 fi
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  zip -r $PACKAGE-$PACKAGE_VALUE-"("Darwin-i386-32")""("Darwin-x86_64-32")""("Sources")"-externals.zip $PACKAGE
+  mv $PACKAGE.zip $PACKAGE-$TRAVIS_COMMIT-"("Darwin-i386-32")""("Darwin-x86_64-32")""("Sources")"-externals.zip
 fi
 elif [["$APPVEYOR" == "True"]]; then
-
+  
 fi
 
 ls
