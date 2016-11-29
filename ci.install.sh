69e2ebc401b1d45d6462358e7f9bd5166ae49eb0
pierreguillot <guillotpierre6@gmail.com>
2016-11-29 15:57:59 +0100

    add script for appveyor

--- ci.install.sh
+++ ci.install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 if [[ "$TRAVIS" == "true" ]]; then
-echo "Build on Travis"
-
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 if [[ "$PLATFORM" == "linux32" ]]; then
+echo "Build on Travis for linux 32"
 sudo apt-get install -y gcc-multilib
 sudo apt-get install -y libc6-dev-i386
 elif [[ "$PLATFORM" == "linux64" ]]; then
-echo "Build on linux 64"
+echo "Build on Travis for linux 64"
 fi
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-echo "Build on osx universal"
+echo "Build on Travis for mac osx universal"
 fi
+elif [["$APPVEYOR" == "True"]]; then
+echo "Build on Appveyor for windows 32"
 fi
--- ci.script.sh
+++ ci.script.sh
@@ -15,4 +15,10 @@ cmake .. "-DCMAKE_OSX_ARCHITECTURES=i386;x86_64"
 fi
 cmake --build . --config Release
 cd ..
+elif [["$APPVEYOR" == "True"]]; then
+mkdir build
+cd build
+cmake .. -G "Visual Studio 14 2015"
+cmake --build . -- /verbosity:minimal /p:Configuration=Release
+cd ..
 fi
