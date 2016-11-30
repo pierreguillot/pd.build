ca84b52a8560264ef68cdc7c6a6ab4c4e27d7d65
pierreguillot <guillotpierre6@gmail.com>
2016-11-30 19:22:51 +0100

    correct tests

--- ci.test.sh
+++ ci.test.sh
@@ -3,13 +3,13 @@
 if [[ "$TRAVIS" == "true" ]]; then
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 if [[ "$PLATFORM" == "linux32" ]]; then
-
+echo "not supported yet"
 elif [[ "$PLATFORM" == "linux64" ]]; then
-
+echo "not supported yet"
 fi
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-curl -o pd-0.47-1.mac.tar.gz 'http://msp.ucsd.edu/Software/pd-0.47-1.mac.tar.gz'
-tar -xvf Pd-0.47-1.tar.gz
-./*.app/Contents/Resources/bin/pd  -stderr -open $1
+  curl -o pd-0.47-1.mac.tar.gz 'http://msp.ucsd.edu/Software/pd-0.47-1.mac.tar.gz'
+  tar -xvf pd-0.47-1.mac.tar.gz
+  ./Pd-0.47-1.app/Contents/Resources/bin/pd  -stderr -open $1
 fi
 fi
