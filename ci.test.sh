ee60ed82a545419cacf791d225856fd72536cfb0
pierreguillot <guillotpierre6@gmail.com>
2016-12-17 09:29:43 +0100

    test

--- ci.test.sh
+++ ci.test.sh
@@ -51,5 +51,6 @@ elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   curl -o pd-0.47-1.mac.tar.gz 'http://msp.ucsd.edu/Software/pd-0.47-1.mac.tar.gz'
   tar -xvf pd-0.47-1.mac.tar.gz
   ./Pd-0.47-1.app/Contents/Resources/bin/pd  -stderr -open $1
+  
 fi
 fi
